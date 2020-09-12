d38731cad52973220095849b6030944d4dc455cc
William De la Cruz <macwilliamdlc@gmail.com>
2020-09-12 12:51:38 -0500

    add react application

---
+++ hitman-ui/src/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+test('it renders login page', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText("Welcome to Hitman");
+  expect(linkElement).toBeInTheDocument();
+});
---
+++ hitman-ui/src/pages/login.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Container, Row, Col, Form, Button} from 'react-bootstrap';
+
+export const Login = () => {
+    return (
+        <Container>
+            <Row className="vertical-center">
+                <Col md={{ span: 4, offset: 4 }}>
+                    <h1>Welcome to Hitman</h1>
+                </Col>
+                <Col md={{ span: 4, offset: 4 }}>
+                    <Form>
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Text className="text-muted">
+                            We'll never share your email with anyone else.
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Form.Group controlId="formBasicCheckbox">
+                            <Form.Check type="checkbox" label="Check me out" />
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    )
+};
