efaee498b09f6f4e0529fd8f100e2763024380c3
William De la Cruz <macwilliamdlc@gmail.com>
2020-09-16 09:25:19 -0500

    add hit model

--- hitman-api/hits/models.py
+++ hitman-api/hits/models.py
@@ -1,11 +1,27 @@
+from cuser.models import CUser
 from django.db import models
-from cuser.models import CUser as User
+
 from hitman.utils.model_mixins import TimeStampedModel
 
 
+class HitStatus(models.TextChoices):
+    ASSIGNED = "ASSIGNED"
+    FAILED = "FAILED"
+    COMPLETED = "COMPLETED"
+
+
 class Hit(TimeStampedModel, models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.TextField(max_length=1024)
+    assigned_to = models.ForeignKey(
+        CUser, on_delete=models.CASCADE, related_name="assigned_to"
+    )
+    target_name = models.CharField(max_length=255, null=False, blank=False)
+    description = models.CharField(max_length=255, null=False, blank=False)
+    status = models.CharField(
+        max_length=9, null=False, choices=HitStatus.choices, default=HitStatus.ASSIGNED
+    )
+    requester = models.ForeignKey(
+        CUser, on_delete=models.CASCADE, related_name="requester"
+    )
 
     class Meta:
         db_table = "hits"
