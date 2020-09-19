7c8b8d68ad1efb047ad2b37ad322a80ae33b2425
William De la Cruz <macwilliamdlc@gmail.com>
2020-09-19 14:30:20 -0500

    add navigation active menu functionality and minor improvents to hits page

--- hitman-ui/src/utils.js
+++ hitman-ui/src/utils.js
@@ -1,8 +1,8 @@
-export const hasHitmenPermissions = (profile) => profile && (profile.roles.includes('manager') || profile.is_super_user);
-export const canAssignHits = (profile) => profile && (profile.roles.includes('manager') || profile.is_super_user);
+export const hasHitmenPermissions = (profile) => profile && (profile.roles.includes('manager') || profile.is_superuser);
+export const canAssignHits = (profile) => profile && (profile.roles.includes('manager') || profile.is_superuser);
 export const isHitman = (user) => Array.from(user.roles, (role) => role.name).includes('hitman');
 export const isManager = (user) => Array.from(user.roles, (role) => role.name).includes('manager');
-export const isBoss = (user) => user.is_super_user || user.is_superuser;
+export const isBoss = (user) => user.is_superuser;
 
 export default {
   hasHitmenPermissions,
